0ef0a8147241c2daa1251e4208cc2cc6d2685a59
jjuarezitc1991 <jjuarezitc1991@gmail.com>
2019-06-29 01:16:39 -0500

    Innitial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule
